05d53b5b72d2aa8fb57352162f96083daea74404
pedroabinajm <pedro.abinajm@easytaxi.com.br>
2019-03-24 22:08:54 -0300

    Move pokedex search pure functions to logic file
    Split pokedex search into more functions to be able to have more pure
    functions

--- src/pokedex/logic/pokedex.clj
+++ src/pokedex/logic/pokedex.clj
@@ -9,3 +9,7 @@
   (into {}
         (let [everyPokemon (flatten (map :pokemon regions))]
           (map-indexed #(new-entry (inc %) %2) everyPokemon))))
+
+(defn search-by-number
+  [number & pokedex]
+  (map (keyword (str number)) pokedex))
